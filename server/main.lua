fbb91c554bf39725c578c339b6eebe2b041b7133
CarlosVergikosk <45525595+CarlosVergikosk@users.noreply.github.com>
2020-02-19 00:11:48 +0000

    Update main.lua

--- server/main.lua
+++ server/main.lua
@@ -2,7 +2,7 @@ ESX = nil
 TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 
 RegisterServerEvent('b1g_chopshop:success')
-AddEventHandler('b1g_chopshop:success', function(pay)
+AddEventHandler('b1g_chopshop:success', function()
 	local xPlayer = ESX.GetPlayerFromId(source)
-	xPlayer.addMoney(pay)
+	xPlayer.addMoney(math.random(10,35))
 end)
